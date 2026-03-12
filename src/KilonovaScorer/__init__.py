4a8425df20617949b9846771291cc91996165ab7
Phelipe Darc <115671737+phelipedarc@users.noreply.github.com>
2026-03-12 16:07:13 -0500

    Import all from core2 in __init__.py

--- src/KilonovaScorer/__init__.py
+++ src/KilonovaScorer/__init__.py
@@ -1,4 +1,5 @@
 from .core import load_observations, kilonovascorer_v1,overlap_chain,preprocess_lsst_like,binned_stats_cumulative_ptail
+from .core2 import *
 from .plotting import plot_final_all_metrics, plot_simulations_LCS
 
 #from .simulation import simulate_kilonova
